bf835697808c9a23d5deed7cc55b80690b4d9c74
Abhishek Pandey <abhishekpandey817827@gmail.com>
2025-07-30 02:35:29 +0530

    [JAR-Assignment] : Added Landing page transition

--- app/src/main/java/com/abhishek/jaronboarding/presentation/ui/screen/OnBoardingScreen.kt
+++ app/src/main/java/com/abhishek/jaronboarding/presentation/ui/screen/OnBoardingScreen.kt
@@ -1,6 +1,9 @@
 package com.abhishek.jaronboarding.presentation.ui.screen
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.EaseInOutCubic
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
@@ -23,16 +26,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewModelScope
 import com.abhishek.jaronboarding.R
 import com.abhishek.jaronboarding.domain.dto.respose.OnBoardingResponse
 import com.abhishek.jaronboarding.presentation.stateHolders.OnBoardingUiState
@@ -40,6 +47,8 @@ import com.abhishek.jaronboarding.presentation.stateHolders.OnBoardingViewModel
 import com.abhishek.jaronboarding.presentation.ui.components.GoldActionButton
 import com.abhishek.jaronboarding.presentation.ui.components.OnboardingFeatureCard
 import com.abhishek.jaronboarding.presentation.ui.components.TopBar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun OnBoardingScreen(
@@ -61,11 +70,19 @@ fun OnBoardingScreen(
 
     val carBackgroundBrush = getCardBackgroundBrush(data, expandedIndex)
 
+    val isSaveClicked = remember { mutableStateOf(false) }
+    val slideOffset by animateFloatAsState(
+        targetValue = if (isSaveClicked.value) 10000f else 0f,
+        animationSpec = tween(500, easing = EaseInOutCubic),
+        label = "SlideOutAnimation"
+    )
+
     Scaffold(topBar = {
         TopBar(
             modifier = Modifier
                 .background(Color.Transparent)
-                .statusBarsPadding(),
+                .statusBarsPadding()
+                .graphicsLayer(translationY = -slideOffset),
             title = stringResource(R.string.onboarding),
             onBackPressed = onBackClick
         )
@@ -83,7 +100,11 @@ fun OnBoardingScreen(
                 ) {
                     GoldActionButton(
                         modifier = Modifier.clickable {
-                            onSaveGoldClick()
+                            isSaveClicked.value = true
+                            viewModel.viewModelScope.launch {
+                                delay(200)
+                                onSaveGoldClick()
+                            }
                         },
                         saveButtonCta = it,
                         ctaLottie = data.manualBuyEducationData?.ctaLottie.orEmpty()
@@ -94,6 +115,7 @@ fun OnBoardingScreen(
     }) { paddingValues ->
         Box(
             modifier = Modifier
+                .graphicsLayer(translationY = -slideOffset)
                 .fillMaxSize()
                 .background(screenBackgroundBrush)
                 .padding(paddingValues),
